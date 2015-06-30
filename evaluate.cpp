de1379897add5f0eec1217416ce789cd3b56ee8f
Chris Hammerschmidt <chrishammerschmidt@ymail.com>
2015-06-30 19:41:43 +0200

    Added metric based merge dummy functions

--- evaluate.cpp
+++ evaluate.cpp
@@ -124,6 +124,57 @@ void overlap_driven::reset(state_merger *merger){
   overlap = 0;
 };
 
+/* Metric driven merging, addition by chrham */
+bool metric_driven::consistent(state_merger *merger, apta_node* left, apta_node* right){
+  if(evaluation_function::consistent(merger,left,right) == false) return false;
+
+  if(left->accepting_paths >= STATE_COUNT){
+    for(int i = 0; i < alphabet_size; ++i){
+      if(right->num_pos[i] >= SYMBOL_COUNT & left->num_pos[i] == 0){
+        inconsistency_found = true;
+        return false;        
+      }
+    }
+  }
+  if(right->accepting_paths >= STATE_COUNT){
+    for(int i = 0; i < alphabet_size; ++i){
+      if(left->num_pos[i] >= SYMBOL_COUNT & right->num_pos[i] == 0){
+        inconsistency_found = true;
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// what's a good score?
+void metric_driven::update_score(state_merger *merger, apta_node* left, apta_node* right){
+  if (inconsistency_found) return;
+  if (consistent(merger, left, right) == false) return;
+  
+  for(int i = 0; i < alphabet_size; ++i){
+    if(left->num_pos[i] != 0 && right->num_pos[i] != 0){
+      overlap += 1;
+    }
+  }
+};
+
+bool metric_driven::compute_consistency(state_merger *merger, apta_node* left, apta_node* right){
+    if(evaluation_function::compute_consistency(merger, left, right) == false) return false;
+    return true;
+};
+
+int metric_driven::compute_score(state_merger *merger, apta_node* left, apta_node* right){
+  return overlap;
+};
+
+void metric_driven::reset(state_merger *merger){
+  inconsistency_found = false;
+  overlap = 0;
+};
+
+/* end addition */
+
 /* ALERGIA, consistency based on Hoeffding bound, merges depth driven */
 bool alergia::consistent(state_merger *merger, apta_node* left, apta_node* right){
   if(depth_driven::consistent(merger,left,right) == false) return false;
