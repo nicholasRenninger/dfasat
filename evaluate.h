bf346faa753546c24de525b687a2a6f5b4b596da
sicco <sicco@siccos-imac.ws.tudelft.net>
2015-10-30 16:51:30 +0100

    bladibla

--- evaluate.h
+++ evaluate.h
@@ -95,8 +95,10 @@ public:
   double merge_error;
   
   virtual bool consistent(state_merger *merger, apta_node* left, apta_node* right);
+  virtual void update_score(state_merger *merger, apta_node* left, apta_node* right);
   virtual int  compute_score(state_merger*, apta_node* left, apta_node* right);
   virtual void reset(state_merger *merger);
+  virtual bool compute_consistency(state_merger *merger, apta_node* left, apta_node* right);
 };
 
 class overlap_driven: public evaluation_function{
