eed562a952cef5e7f2aafd773df0fb615620cfaf
Chris Hammerschmidt <chrishammerschmidt@ymail.com>
2016-10-24 14:47:44 +0200

    modified the parameter class to have consistent parameter names between the command line libpopt interface and the estimator class constructor

--- main.cpp
+++ main.cpp
@@ -38,31 +38,31 @@ public:
     string sat_program;
     string hName;
     string hData;
-    int tries;
+    int runs;
     int sinkson;
     int seed;
-    int apta_bound;
-    int dfa_bound;
+    int sataptabound;
+    int satdfabound;
     float lower_bound;
-    int offset;
-    int merge_sinks_p;
-    int merge_sinks_d;
+    int satextra;
+    int mergesinks;
+    int satmergesinks;
     int method;
     int extend;
     int heuristic;
     int symbol_count;
     int state_count;
     float correction;
-    float parameter;
-    int extra_states;
-    int target_rejecting;
+    float extrapar;
+    int satplus;
+    int satfinalred;
     int symmetry;
     int forcing;
     int blueblue;
-    int fixedred;
+    int finalred;
     int largestblue;
     int testmerge;
-    int depthfirst;
+    int shallowfirst;
     
     parameters();
 };
@@ -72,31 +72,31 @@ parameters::parameters(){
     sat_program = "";
     hName = "default";
     hData = "evaluation_data";
-    tries = 1;
+    runs = 1;
     sinkson = 1;
     seed = 12345678;
-    apta_bound = 2000;
-    dfa_bound = 50;
-    merge_sinks_p = 1;
-    merge_sinks_d = 0;
+    sataptabound = 0;
+    satdfabound = 50;
+    mergesinks = 1;
+    satmergesinks = 0;
     lower_bound = -1.0;
-    offset = 5;
+    satextra = 5;
     method=1;
     heuristic=1;
     extend=1;
     symbol_count = 10;
     state_count = 25;
     correction = 0.0;
-    parameter = 0.5;
-    extra_states = 0;
-    target_rejecting = 0;
+    extrapar = 0.5;
+    satplus = 0;
+    satfinalred = 0;
     symmetry = 1;
     forcing = 0;
     blueblue = 0;
-    fixedred = 0;
+    finalred = 0;
     largestblue = 0;
     testmerge = 0;
-    depthfirst =0;
+    shallowfirst =0;
 };
 
 
@@ -105,33 +105,33 @@ void init_with_params(parameters* param) {
 
     srand(param->seed);
     
-    APTA_BOUND = param->apta_bound;
-    CLIQUE_BOUND = param->dfa_bound;
+    APTA_BOUND = param->sataptabound;
+    CLIQUE_BOUND = param->satdfabound;
     
     STATE_COUNT = param->state_count;
     SYMBOL_COUNT = param->symbol_count;
-    CHECK_PARAMETER = param->parameter;
+    CHECK_PARAMETER = param->extrapar;
     CORRECTION = param->correction;
 
     LOWER_BOUND = param->lower_bound;
-    OFFSET = param->offset;
+    OFFSET = param->satextra;
     USE_SINKS = param->sinkson;
-    MERGE_SINKS_PRESOLVE = param->merge_sinks_p;
-    MERGE_SINKS_DSOLVE = param->merge_sinks_d;
+    MERGE_SINKS_PRESOLVE = param->mergesinks;
+    MERGE_SINKS_DSOLVE = param->satmergesinks;
     EXTEND_ANY_RED = param->extend;
     
     SYMMETRY_BREAKING = param->symmetry;
     FORCING = param->forcing;
         
-    EXTRA_STATES = param->extra_states;
-    TARGET_REJECTING = param->target_rejecting;
+    EXTRA_STATES = param->satplus;
+    TARGET_REJECTING = param->satfinalred;
 
     /* new since master-dev merge */
     MERGE_MOST_VISITED = param->largestblue;
     MERGE_BLUE_BLUE = param->blueblue;
-    RED_FIXED = param->fixedred;
+    RED_FIXED = param->finalred;
     MERGE_WHEN_TESTING = !param->testmerge;
-    DEPTH_FIRST = param->depthfirst;
+    DEPTH_FIRST = param->shallowfirst;
 
 
     if(param->method == 1) GREEDY_METHOD = RANDOMG;
@@ -187,7 +187,7 @@ void run(parameters* param) {
     merger.print_dot(output);
     fclose(output);
     
-    for(int i = 0; i < param->tries; ++i){
+    for(int i = 0; i < param->runs; ++i){
         std::ostringstream oss;
         oss << param->dot_file << (i+1) << ".aut";
         std::ostringstream oss2;
@@ -245,26 +245,26 @@ int main(int argc, const char *argv[]){
         { "data-name", 'd', POPT_ARG_STRING, &(hData), 'd', "Name of the merge data class to use; default count_data. Use any heuristic in the evaluation directory.", "string" },
         { "method", 'm', POPT_ARG_INT, &(param->method), 'm', "Method to use when merging states, default value 1 is random greedy (used in Stamina winner), 2 is one standard (non-random) greedy.", "integer" },
         { "seed", 's', POPT_ARG_INT, &(param->seed), 's', "Seed for random merge heuristic; default=12345678", "integer" },
-        { "runs", 'n', POPT_ARG_INT, &(param->tries), 'n', "Number of random greedy runs/iterations; default=1. Advice: when using random greedy, a higher value is recommended (100 was used in Stamina winner).\n\nSettings that modify the red-blue state-merging framework:", "integer" },
+        { "runs", 'n', POPT_ARG_INT, &(param->runs), 'n', "Number of random greedy runs/iterations; default=1. Advice: when using random greedy, a higher value is recommended (100 was used in Stamina winner).\n\nSettings that modify the red-blue state-merging framework:", "integer" },
         { "extend", 'x', POPT_ARG_INT, &(param->extend), 'x', "When set to 1, any merge candidate (blue) that cannot be merged with any target (red) is immediately changed into a (red) target; default=1. If set to 0, a merge candidate is only changed into a target when no more merges are possible. Advice: unclear which strategy is best, when using statistical (or count-based) consistency checks, keep in mind that merge consistency between states may change due to other performed merges. This will especially influence low frequency states. When there are a lot of those, we therefore recommend setting x=0.", "integer" },
-        { "shallowfirst", 'w', POPT_ARG_INT, &(param->depthfirst), 'w', "When set to 1, the ordering of the nodes is changed from most frequent first (default) to most shallow (smallest depth) first; default=0. Advice: use depth-first when learning from characteristic samples.", "integer" },
+        { "shallowfirst", 'w', POPT_ARG_INT, &(param->shallowfirst), 'w', "When set to 1, the ordering of the nodes is changed from most frequent first (default) to most shallow (smallest depth) first; default=0. Advice: use depth-first when learning from characteristic samples.", "integer" },
         { "largestblue", 'a', POPT_ARG_INT, &(param->largestblue), 'a', "When set to 1, the algorithm only tries to merge the most frequent (or most shallow if w=1) candidate (blue) states with any target (red) state, instead of all candidates; default=0. Advice: this reduces run-time significantly but comes with a potential decrease in merge quality.", "integer" },
         { "blueblue", 'b', POPT_ARG_INT, &(param->blueblue), 'b', "When set to 1, the algorithm tries to merge candidate (blue) states with candiate (blue) states in addition to candidate (blue) target (red) merges; default=0. Advice: this adds run-time to the merging process in exchange for potential improvement in merge quality.", "integer" },
-        { "finalred", 'f', POPT_ARG_INT, &(param->fixedred), 'f', "When set to 1, merges that add new transitions to red states are considered inconsistent. Merges with red states will also not modify any of the counts used in evaluation functions. Once a red state has been learned, it is considered final and unmodifiable; default=0. Advice: setting this to 1 frequently results in easier to vizualize and more insightful models.\n\nSettings that influece the use of sinks:", "integer" },
-        { "sinks", 'I', POPT_ARG_INT, &(param->sinkson), 'I', "Set to 1 to use sink states; default=1. Advice: leads to much more concise and easier to vizualize models, but can cost predictive performance depending on the sink definitions.", "integer"},
-        { "mergesinks", 'J', POPT_ARG_INT, &(param->merge_sinks_d), 'J', "Sink nodes are candidates for merging during the greedy runs (setting 0 or 1); default=0. Advice: merging sinks typically only makes the learned model worse. Keep in mind that sinks can become non-sinks due to other merges that influcence the occurrence counts.", "integer" },
-        { "satmergesinks", 'K', POPT_ARG_INT, &(param->merge_sinks_p), 'K', "Merge all sink nodes of the same type before sending the problem to the SAT solver (setting 0 or 1); default=1. Advice: radically improves runtime, only set to 0 when sinks of the same type can be different states in the final model.\n\nSettings that influence merge evaluations:", "integer" },
+        { "finalred", 'f', POPT_ARG_INT, &(param->finalred), 'f', "When set to 1, merges that add new transitions to red states are considered inconsistent. Merges with red states will also not modify any of the counts used in evaluation functions. Once a red state has been learned, it is considered final and unmodifiable; default=0. Advice: setting this to 1 frequently results in easier to vizualize and more insightful models.\n\nSettings that influece the use of sinks:", "integer" },
+        { "sinkson", 'I', POPT_ARG_INT, &(param->sinkson), 'I', "Set to 1 to use sink states; default=1. Advice: leads to much more concise and easier to vizualize models, but can cost predictive performance depending on the sink definitions.", "integer"},
+        { "mergesinks", 'J', POPT_ARG_INT, &(param->mergesinks), 'J', "Sink nodes are candidates for merging during the greedy runs (setting 0 or 1); default=0. Advice: merging sinks typically only makes the learned model worse. Keep in mind that sinks can become non-sinks due to other merges that influcence the occurrence counts.", "integer" },
+        { "satmergesinks", 'K', POPT_ARG_INT, &(param->satmergesinks), 'K', "Merge all sink nodes of the same type before sending the problem to the SAT solver (setting 0 or 1); default=1. Advice: radically improves runtime, only set to 0 when sinks of the same type can be different states in the final model.\n\nSettings that influence merge evaluations:", "integer" },
         { "testmerge", 't', POPT_ARG_INT, &(param->testmerge), 't', "When set to 1, merge tries in order to compute the evaluation scores do not actually perform the merges themselves. Thus the consistency and score evaluation for states in merges that add recursive loops are uninfluenced by earlier merges; default=0. Advice: setting this to 1 reduces run-time and can be useful when learning models using statistical evaluation functions, but can lead to inconsistencies when learning from labeled data.", "integer" },
         { "lowerbound", 'l', POPT_ARG_FLOAT, &(param->lower_bound), 'l', "Minimum value of the heuristic function, smaller values are treated as inconsistent, also used as the paramater value in any statistical tests; default=-1. Advice: state merging is forced to perform the merge with best heuristic value, it can sometimes be better to color a state red rather then performing a bad merge. This is achieved using a positive lower bound value. Models learned with positive lower bound are frequently more interpretable.", "float" },
-        { "statecount", 'q', POPT_ARG_INT, &(param->state_count), 'q', "The minimum number of positive occurrences of a state for it to be included in overlap/statistical checks (see evaluation functions); default=25. Advice: low frequency states can have an undesired influence on statistical tests, set to at least 10. Note that different evaluation functions can use this parameter in different ways.", "integer" },
-        { "symbolcount", 'y', POPT_ARG_INT, &(param->symbol_count), 'y', "The minimum number of positive occurrences of a symbol/transition for it to be included in overlap/statistical checks, symbols with less occurrences are binned together; default=10. Advice: low frequency transitions can have an undesired influence on statistical tests, set to at least 4. Note that different evaluation functions can use this parameter in different ways.", "integer" },
+        { "state_count", 'q', POPT_ARG_INT, &(param->state_count), 'q', "The minimum number of positive occurrences of a state for it to be included in overlap/statistical checks (see evaluation functions); default=25. Advice: low frequency states can have an undesired influence on statistical tests, set to at least 10. Note that different evaluation functions can use this parameter in different ways.", "integer" },
+        { "symbol_count", 'y', POPT_ARG_INT, &(param->symbol_count), 'y', "The minimum number of positive occurrences of a symbol/transition for it to be included in overlap/statistical checks, symbols with less occurrences are binned together; default=10. Advice: low frequency transitions can have an undesired influence on statistical tests, set to at least 4. Note that different evaluation functions can use this parameter in different ways.", "integer" },
         { "correction", 'c', POPT_ARG_FLOAT, &(param->correction), 'c', "Value of a Laplace correction (smoothing) added to all symbol counts when computing statistical tests (in ALERGIA, LIKELIHOODRATIO, AIC, and KULLBACK-LEIBLER); default=0.0. Advice: unclear whether smoothing is needed for the different tests, more smoothing typically leads to smaller models.", "float" },
-        { "extrapar", 'p', POPT_ARG_FLOAT, &(param->parameter), 'p', "Extra parameter used during statistical tests, the significance level for the likelihood ratio test, the alpha value for ALERGIA; default=0.5. Advice: look up the statistical test performed, this parameter is not always the same as a p-value.\n\nSettings influencing the SAT solving procedures:", "float" },
-        { "sataptabound", 'A', POPT_ARG_INT, &(param->apta_bound), 'A', "Maximum number of remaining states in the partially learned DFA before starting the SAT search process. The higher this value, the larger the problem sent to the SAT solver; default=2000. Advice: try sending problem instances that are as large as possible, since larger instances take more time, test what time is acceptable for you.", "integer" },
-        { "satdfabound", 'D', POPT_ARG_INT, &(param->dfa_bound), 'D', "Maximum size of the partially learned DFA before starting the SAT search process; default=50. Advice: when the merging process performs bad merges, it can blow-up the size of the learned model. This test ensres that models that are too large do not get solved.", "integer" },
-        { "satextra", 'E', POPT_ARG_INT, &(param->offset), 'E', "DFASAT runs a SAT solver to find a solution of size at most the size of the partially learned DFA + E; default=5. Advice: larger values greatly increases run-time. Setting it to 0 is frequently sufficient (when the merge heuristic works well).", "integer" },
-        { "satplus", 'P', POPT_ARG_INT, &(param->extra_states), 'P', "With every iteration, DFASAT tries to find solutions of size at most the best solution found + P, default=0. Advice: current setting only searches for better solutions. If a few extra states is OK, set it higher.", "int" },
-        { "satfinalred", 'F', POPT_ARG_INT, &(param->target_rejecting), 'F', "Make all transitions from red states without any occurrences force to have 0 occurrences (similar to targeting a rejecting sink), (setting 0 or 1) before sending the problem to the SAT solver; default=0. Advice: the same as finalred but for the SAT solver. Setting it to 1 greatly improves solving speed.", "integer" },
+        { "extrapar", 'p', POPT_ARG_FLOAT, &(param->extrapar), 'p', "Extra parameter used during statistical tests, the significance level for the likelihood ratio test, the alpha value for ALERGIA; default=0.5. Advice: look up the statistical test performed, this parameter is not always the same as a p-value.\n\nSettings influencing the SAT solving procedures:", "float" },
+        { "sataptabound", 'A', POPT_ARG_INT, &(param->sataptabound), 'A', "Maximum number of remaining states in the partially learned DFA before starting the SAT search process. The higher this value, the larger the problem sent to the SAT solver; default=2000. Advice: try sending problem instances that are as large as possible, since larger instances take more time, test what time is acceptable for you.", "integer" },
+        { "satdfabound", 'D', POPT_ARG_INT, &(param->satdfabound), 'D', "Maximum size of the partially learned DFA before starting the SAT search process; default=50. Advice: when the merging process performs bad merges, it can blow-up the size of the learned model. This test ensres that models that are too large do not get solved.", "integer" },
+        { "satextra", 'E', POPT_ARG_INT, &(param->satextra), 'E', "DFASAT runs a SAT solver to find a solution of size at most the size of the partially learned DFA + E; default=5. Advice: larger values greatly increases run-time. Setting it to 0 is frequently sufficient (when the merge heuristic works well).", "integer" },
+        { "satplus", 'P', POPT_ARG_INT, &(param->satplus), 'P', "With every iteration, DFASAT tries to find solutions of size at most the best solution found + P, default=0. Advice: current setting only searches for better solutions. If a few extra states is OK, set it higher.", "int" },
+        { "satfinalred", 'F', POPT_ARG_INT, &(param->satfinalred), 'F', "Make all transitions from red states without any occurrences force to have 0 occurrences (similar to targeting a rejecting sink), (setting 0 or 1) before sending the problem to the SAT solver; default=0. Advice: the same as finalred but for the SAT solver. Setting it to 1 greatly improves solving speed.", "integer" },
         { "satsymmetry", 'Y', POPT_ARG_INT, &(param->symmetry), 'Y', "Add symmetry breaking predicates to the SAT encoding (setting 0 or 1), based on Ulyantsev et al. BFS symmetry breaking; default=1. Advice: in our experience this only improves solving speed.", "integer" },
         { "satonlyinputs", 'O', POPT_ARG_INT, &(param->forcing), 'O', "Add predicates to the SAT encoding that force transitions in the learned DFA to be used by input examples (setting 0 or 1); default=0. Advice: leads to non-complete models. When the data is sparse, this should be set to 1. It does make the instance larger and can have a negative effect on the solving time.", "integer" },
         POPT_AUTOHELP
