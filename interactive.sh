ac520ba393f3ff67ae5da4b6325b7af5e78744e3
Chris Hammerschmidt <chrishammerschmidt@ymail.com>
2017-05-28 12:09:47 +0200

    updated shell scripts

---
+++ interactive.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# requires inotify-tool package
+# based on Jonathan Hartley's snippet at https://superuser.com/questions/181517/how-to-execute-a-command-whenever-a-file-changes
+
+function execute() {
+    clear
+    echo "$@"
+    eval "dot -Tpdf $@ > $@.pdf"
+}
+
+
+inotifywait --quiet --recursive --monitor --event close_write --format "%w%f" ./tests/ \
+| while read change; do
+    if [[ "$change" == *.dot ]] 
+    then
+      execute "$change"
+    fi
+done
