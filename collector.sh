d6c167025999a175e74c510cece3583bf01835b6
Nicholas Renninger <nire1188@colorado.edu>
2020-02-04 15:47:35 -0700

    reorganizing evaluation headers in ./src ./src/lib format for consistency

--- collector.sh
+++ collector.sh
@@ -2,7 +2,7 @@ HEADER=lib/evaluators.h
 rm $HEADER
 echo "#ifndef __ALL_HEADERS__" > $HEADER
 echo "#define __ALL_HEADERS__" >> $HEADER
-for file in evaluation/*.h
+for file in evaluation/lib/*.h
 do
     echo "#include \"$file\"" >> $HEADER
 done
